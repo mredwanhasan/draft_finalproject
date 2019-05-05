b1664a2b64d2fd8289a51fa0e43073141b419687
mredwanhasan <m.redwanhasan@hotmail.com>
2019-05-05 19:01:02 +0200

    Update - 5th May

--- draft_finalproject/draft_codes.R
+++ draft_finalproject/draft_codes.R
@@ -1,6 +1,8 @@
+install.packages("ggrepel")
 library(dplyr)
 library(tidyverse)
 library(kableExtra)
+library(ggrepel)
 
 as_tibble(Champions_Info)
 as_tibble(League_Averages)
@@ -8,10 +10,21 @@ as_tibble(Player_Data)
 as_tibble(Players)
 as_tibble(Seasons_Stats)
 
-Player_Data %>% select(-height) %>% filter(year_start > 1990, year_end < 2000)
+# Here I tried to mutate a new column by calculating percentage of players per state. This is arrangd in descending order)
 a = Players %>% count(birth_state) %>% arrange(desc(n)) %>% mutate(percent_players = (n/sum(n)*100))                    
 
-a %>% slice(2:11) %>% ggplot() + geom_bar(mapping = aes(x = birth_state, y = percent_players), stat = "identity", fill = ) + 
-  ggtitle("Total player representation by state")
+# A plot to visualize the previous line of code 
+a %>% slice(2:11) %>% ggplot() + 
+  geom_bar(mapping = aes(x = reorder(birth_state, -percent_players), y = percent_players, fill = birth_state), stat = "identity") + 
+  ggtitle("Total player representation by state") + xlab("State")
 
- 
+# Here we calculate the mean height and weight per birth_state. You can also rearrange it to see.
+Players %>% group_by(birth_state) %>% summarize(mean_height_per_state = mean(height, na.rm = TRUE),
+                                                mean_weight_per_state = mean(weight, na.rm = TRUE))
+
+# Made variables by selecting certain columns
+b = Player_Data %>% select(year_start, year_end, position, name)
+c = Seasons_Stats %>% select(Player, Age, Tm)
+
+# This is where we join different tables. 
+b %>% left_join(Players, by = c("name" = "Player")) %>% left_join(c, by = c("name" = "Player"))
